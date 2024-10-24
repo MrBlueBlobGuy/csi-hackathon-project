af3ed5d344b31e394f58e7771faac8bd0646892e
MrBlueBlobGuy <aternoshostingmc@gmail.com>
2024-10-24 19:28:07 +0530

    courses api working

---
+++ csi_project/coursetablehandler.py
@@ -0,0 +1,50 @@
+import mysql.connector as mysql
+from dotenv import load_dotenv
+
+import uuid
+
+load_dotenv()
+
+import os
+
+conn = mysql.connect(
+    host = "localhost",
+    user = "root",
+    password = os.getenv("PASSWORD"),
+    database = os.getenv("DATABASE")
+)
+
+cursor = conn.cursor()
+
+def add_course(coursename, teacher, description):
+    try:
+        luuid = str(uuid.UUID().bytes)
+        cursor.execute("INSERT INTO courses (courseNAME, toughtBY, descrip, courseID) VALUES (%s, %s, %s, %s)", [coursename, teacher, description, luuid])
+        cursor._connection.commit()
+    except mysql.Error as error:
+        print(f"CRITICAL ERROR OCCURED AT : {error}")
+
+def find_courses(course_id=None, name=None, teacher=None, description=None):
+    query = "SELECT * FROM courses WHERE 1=1"
+    params = []
+    
+    if course_id:
+        query += " AND courseID = %s"
+        params.append(course_id)
+    if name:
+        query += " AND courseNAME = %s"
+        params.append(name)
+    if teacher:
+        query += " AND toughtBY = %s"
+        params.append(teacher)
+    if description:
+        query += " AND descrip = %s"
+        params.append(description)
+    
+    try:
+        cursor.execute(query, params)
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+    except mysql.connector.Error as error:
+        print(f"Error occurred: {error}")
--- csi_project/routes/users.py
+++ csi_project/routes/users.py
@@ -2,24 +2,40 @@ from fastapi import APIRouter
 from ..usertablemanager import *
 from pydantic import BaseModel
 
-import enum
 import uuid
+import json
 
 class UserLoginModel(BaseModel):
     user:str
     hashedpassword:str
 
+class UserSignupModel(BaseModel):
+    username:str
+    hashedsaltedpassword:str
+    salt:str
+    usertype:int
+
 userrouter = APIRouter(
     prefix='/users'
 )
 
-@userrouter.post('/getsalt')
-def a():
-    pass
+@userrouter.get('/getsalt')
+def getsalt(username):
+    return getsaltfromdb(username, cursor)
 
 @userrouter.post('/login')
 async def login(UserModel:UserLoginModel):
     try:    
-        return f"response: {str(verifylogininfo(UserModel.user, UserModel.hashedpassword, cursor))}"
+        return json.dumps({"response": str(verifylogininfo(UserModel.user, UserModel.hashedpassword, cursor))})
     except Exception as err:
         print(err)
+
+@userrouter.get("/generatenewsalt")
+def generatenewsalt():
+    id = uuid.UUID()
+    return "{}"
+
+@userrouter.post("/signup")
+def signup(signupmodel:UserSignupModel):
+    add_entry(signupmodel.username, signupmodel.hashedsaltedpassword, str(uuid.UUID()))
+
